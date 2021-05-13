ed44003b48f835196dfb25de89955ec2dce8b269
Gregory Maginn <gregorymaginn@Gregorys-Mac-mini.local>
2021-05-13 12:51:00 -0400

    creating function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,23 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", sortTable);
+form.on("submit",sortTable);
+
+function sortTable () {
+    //stops page from refreshing
+    d3.event.preventDefault();
+
+    //assign user input to var
+    var userInput = d3.select("#date-input");
+
+    var inputValue = userInput.property("value");
+    
+};
