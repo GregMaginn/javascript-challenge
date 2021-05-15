81aae525a6a9617a03ae8a17a4462817b1f1e8e1
Gregory Maginn <gregorymaginn@Gregorys-Mac-mini.local>
2021-05-15 16:52:58 -0400

    functions almost done

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,8 +1,33 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-// Select the button
+var tbody = d3.select("tbody");
+
+// add a new row for every entry in data
+tableData.forEach(entry => {
+    var row = tbody.append('tr');
+    //add a new cell and append the data into it
+    Object.values(entry).forEach(value => {
+        var cell = row.append('td')
+        cell.text(value)
+    })
+});
+
+var button = d3.select('#filter-btn');
+var form = d3.select('#form')
+
+button.on('click', handleClick);
+form.on('click', handleClick);
+
+function handleClick(){
+    //default is to refresh, stop that
+    d3.event.preventDefault();
+
+    var userInput = d3.select("#datetime");
+    var inputValue = userInput.property("value");
+
+    var userFilter = tableData.filter(dat)
+}
 var button = d3.select("#filter-btn");
 console.log(button)
 // Select the form
@@ -17,10 +42,10 @@ function sortTable () {
     d3.event.preventDefault();
 
     //assign user input to var
-    var userInput = d3.select("#datetime");
+    
 
     //get value from userInput
-    var inputValue = userInput.property("value");
+    
 
     //checking
     console.log(userInput);
